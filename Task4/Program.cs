5c35e37ed00166b1d9526f8c69931ec12386b605
Olexandr Dubchak <a.dubchak@disoft.dev>
2024-03-05 21:50:34 +0200

    added reading from file

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,6 +6,42 @@ class Program
 {
     static void Main(string[] args)
     {
+        var inputPath = Path.Combine(new[]{"..", "..", "..", "files", "expressions.txt"});
+        var outputPath = Path.Combine(new[]{"..", "..", "..", "files", "expressionsOutput.txt"});
+        CalculateFromFile(inputPath, outputPath);
+
+        GetUserInputCalculations();
+    }
+
+    private static void CalculateFromFile(string inputPath, string outputPath)
+    {
+        var newFileLines = new List<string>(); 
+        foreach (var line in File.ReadLines(inputPath))
+        {
+            string result;
+            try
+            {
+                var calculator = new Calculator(line);
+                result = calculator.Evaluate().ToString();
+            }
+            catch (Exception ex) when (ex is ArgumentException or ApplicationException or DivideByZeroException)
+            {
+                result = ex.Message;
+            }
+
+            var newLine = line + " = " + result;
+            newFileLines.Add(newLine);
+        }
+        
+        using (StreamWriter outputFile = new StreamWriter(outputPath))
+        {
+            foreach (string line in newFileLines)
+                outputFile.WriteLine(line);
+        }
+    }
+    
+    private static void GetUserInputCalculations()
+    {
         Console.WriteLine("_____Calculator_____");
         while (true)
         {
@@ -28,7 +64,5 @@ class Program
             }
 
         }
-
-
     }
 }
