0d406c907e588887f15fa14e492f694e1a5eaba0
Olexandr Dubchak <a.dubchak@disoft.dev>
2024-03-11 18:35:40 +0200

    improved interaction with program

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,20 +1,60 @@
-﻿using System.Text.RegularExpressions;
-
-namespace Task4;
+﻿namespace Task4;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var inputPath = Path.Combine(new[]{"..", "..", "..", "files", "expressions.txt"});
-        var outputPath = Path.Combine(new[]{"..", "..", "..", "files", "expressionsOutput.txt"});
-        CalculateFromFile(inputPath, outputPath);
-
-        GetUserInputCalculations();
+        Console.WriteLine(
+            "This is a simple calculator, which evaluates expressions. \n" + 
+            "To start, choose a mode to work: \n" +
+            "'1' is for console mode, to calculate in real-time\n" +
+            "'2' is for file mode, which calculates expressions from the file\n" +
+            "'3' to exit program");
+        var shouldExit = false;
+        do
+        {
+            Console.WriteLine("Enter mode: ");
+            var userInput = Console.ReadLine();
+            int.TryParse(userInput, out int mode);
+            switch (mode)
+            {
+                case 1:
+                    GetUserInputCalculations();
+                    break;
+                case 2:
+                    CalculateFromFile();
+                    break;
+                case 3:
+                    shouldExit = true;
+                    break;
+                default:
+                    Console.WriteLine("Given mode is not valid");
+                    break;
+            }
+        } while (!shouldExit);
     }
 
-    private static void CalculateFromFile(string inputPath, string outputPath)
+    private static void CalculateFromFile()
     {
+        Console.WriteLine("__________file mode__________");
+        Console.WriteLine("To exit, type 'exit'");
+        
+        Console.WriteLine("Enter a path to file: ");
+        string inputPath = Console.ReadLine();
+        if (inputPath == "exit") return;
+        if (!File.Exists(inputPath))
+        {
+            Console.WriteLine("Given path to file does not exist");
+        }
+        
+        Console.WriteLine("Enter a directory path to output file: ");
+        string outputDirectory = Console.ReadLine();
+        if (outputDirectory == "exit") return;
+        if (!Directory.Exists(inputPath))
+        {
+            Console.WriteLine("Given directory path does not exist");
+        }
+        
         var newFileLines = new List<string>(); 
         foreach (var line in File.ReadLines(inputPath))
         {
@@ -32,25 +72,31 @@ class Program
             var newLine = line + " = " + result;
             newFileLines.Add(newLine);
         }
-        
-        using (StreamWriter outputFile = new StreamWriter(outputPath))
+
+        var outputFilePath = Path.Combine(outputDirectory, $"computed_{DateTime.Now}.txt");
+        using (StreamWriter outputFile = new StreamWriter(outputFilePath))
         {
             foreach (string line in newFileLines)
                 outputFile.WriteLine(line);
         }
+        Console.WriteLine($"File was created at {outputFilePath}");
     }
     
     private static void GetUserInputCalculations()
     {
         Console.WriteLine("_____Calculator_____");
+        Console.WriteLine("To exit, type 'exit'");
         while (true)
         {
             try
             {
                 Console.WriteLine("Enter an expression: ");
                 string expr = Console.ReadLine();
+                if (expr == "exit")
+                {
+                    break;
+                }
                 var rvp = new Calculator(expr);
-
                 rvp.PrintReversedPolishNotation();
          
                 var result = rvp.Evaluate();
